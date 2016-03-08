6233bfa927dc68220bede50cec120056dfea505b
Bence <bgazsi94@gmail.com>
2016-03-08 18:11:09 +0100

    -- webstorm git test

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 var express = require('express');
-var app = express();
-var expressHbs = require('express-handlebars');
+var app     = express();
+var path    = require("path");
 
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-
+/*
 var performers = [
     {name: 'Hobo Blues Band'},
     {name: 'Ed Sheeran'},
     {name: 'Jimmy Hendrix'}
   ];
-  
+
   var songs = [
     {title: '45-ös blues', year: '1982', performer: 'Hobo Blues Band', length: '5:31', genre: 'blues'},
     {title: 'Foxy Lady', year: '1970', performer: 'Jimmy Hendrix', length: '2:28', genre: 'blues'},
     {title: 'Sing', year: '2013', performer: 'Ed Sheeran', length: '4:27', genre: 'pop'},
     {title: 'Édes otthon', year: '1982', performer: 'Hobo Blues Band', length: '3:31', genre: 'blues'}
   ];
-  
+
   var data = {
     performers: performers,
     songs: songs
   };
-  res.render('index', data);
+  res.render('index', data);*/
+    res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-app.get('/performer', function(req, res) {
-
+app.get('/songs', function(req, res) {
+/*
     var songs = [
         {title: '45-ös blues', year: '1982', performer: 'Hobo Blues Band', length: '5:31', genre: 'blues'},
         {title: 'Édes otthon', year: '1982', performer: 'Hobo Blues Band', length: '3:31', genre: 'blues'}
     ];
 
     var data = {songs: songs};
-    res.render('performer', data);
+    res.render('songs', data);*/
+    res.sendFile(path.join(__dirname+'/views/songs.html'));
+});
+
+app.get('/performers', function(req, res) {
+/*
+    var songs = [
+        {title: '45-ös blues', year: '1982', performer: 'Hobo Blues Band', length: '5:31', genre: 'blues'},
+        {title: 'Édes otthon', year: '1982', performer: 'Hobo Blues Band', length: '3:31', genre: 'blues'}
+    ];
+
+    var data = {songs: songs};
+    res.render('performers', data);*/
+    res.sendFile(path.join(__dirname+'/views/performers.html'));
 });
 
 app.listen(3000);
