872eb6276a234efadcfdf7c1a52aea64176f3e9a
BGazsi <bgazsi94@gmail.com>
2016-04-09 10:04:15 +0200

    ejs templating

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app     = express();
 var path    = require("path");
 
 app.use(express.static(__dirname + '/public'));
-
+app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-/*
+
 var performers = [
     {name: 'Hobo Blues Band'},
     {name: 'Ed Sheeran'},
@@ -23,36 +23,37 @@ var performers = [
     performers: performers,
     songs: songs
   };
-  res.render('index', data);*/
-    res.sendFile(path.join(__dirname+'/views/index.html'));
+  res.render('index', data);
+    res.sendFile(path.join(__dirname+'/views/index.ejs'));
 });
 
 app.get('/songs', function(req, res) {
-/*
+
     var songs = [
         {title: '45-ös blues', year: '1982', performer: 'Hobo Blues Band', length: '5:31', genre: 'blues'},
         {title: 'Édes otthon', year: '1982', performer: 'Hobo Blues Band', length: '3:31', genre: 'blues'}
     ];
 
     var data = {songs: songs};
-    res.render('songs', data);*/
-    res.sendFile(path.join(__dirname+'/views/songs.html'));
+    res.render('songs', data);
+    res.sendFile(path.join(__dirname+'/views/songs.ejs'));
 });
 
 app.get('/performers', function(req, res) {
-/*
+
     var songs = [
         {title: '45-ös blues', year: '1982', performer: 'Hobo Blues Band', length: '5:31', genre: 'blues'},
         {title: 'Édes otthon', year: '1982', performer: 'Hobo Blues Band', length: '3:31', genre: 'blues'}
     ];
 
     var data = {songs: songs};
-    res.render('performers', data);*/
-    res.sendFile(path.join(__dirname+'/views/performers.html'));
+    res.render('performers', data);
+    res.sendFile(path.join(__dirname+'/views/performers.ejs'));
 });
 
 app.get('/login', function(req, res) {
-    res.sendFile(path.join(__dirname+'/views/login.html'));
+    res.render('login');
+    res.sendFile(path.join(__dirname+'/views/login.ejs'));
 });
 
 app.listen(3000);
